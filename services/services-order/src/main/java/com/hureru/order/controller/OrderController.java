d0e4d2aedc114aff0b2fe1bddc0aecdf835ea57b
Hureru <121702350+Hureru@users.noreply.github.com>
2025-07-14 00:08:16 +0800

    add Sentinel 热点限流规则API

--- services/services-order/src/main/java/com/hureru/order/controller/OrderController.java
+++ services/services-order/src/main/java/com/hureru/order/controller/OrderController.java
@@ -1,5 +1,7 @@
 package com.hureru.order.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.hureru.order.bean.Order;
 import com.hureru.order.properties.OrderProperties;
 import com.hureru.order.service.Impl.OrderServiceImpl;
@@ -9,6 +11,8 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
 
 //@RefreshScope // 配置自动刷新功能
 @RestController
@@ -34,5 +38,21 @@ public class OrderController {
     @GetMapping("/order")
     public Order getOrder(@RequestParam("userId") Long userId, @RequestParam("productId") Long productId) {
         return orderServiceImpl.getOrderByUserIdAndProductId(userId , productId);
+    }
+
+    @GetMapping("/seckill")
+    @SentinelResource(value = "seckill-order", fallback = "seckillFallback")
+    public Order seckill(@RequestParam("userId") Long userId, @RequestParam("productId") Long productId) {
+        return orderServiceImpl.getOrderByUserIdAndProductId(userId , Long.MAX_VALUE);
+    }
+
+    public Order seckillFallback(Long userId, Long productId, BlockException e) {
+        Order order = new Order();
+        order.setId(0);
+        order.setTotalAmount(new BigDecimal("0"));
+        order.setUserId(userId);
+        order.setNickName("未知用户");
+        order.setAddress("异常原因："+e.getClass());
+        return order;
     }
 }
