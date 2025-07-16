f1d28d09b5d00b7a54b211954563106268641563
Hureru <121702350+Hureru@users.noreply.github.com>
2025-07-16 18:42:55 +0800

    add init Seata project

---
+++ services/seata-business/src/main/java/com/atguigu/business/service/impl/BusinessServiceImpl.java
@@ -0,0 +1,24 @@
+package com.atguigu.business.service.impl;
+
+import com.atguigu.business.feign.OrderFeignClient;
+import com.atguigu.business.feign.StorageFeignClient;
+import com.atguigu.business.service.BusinessService;
+import org.apache.seata.spring.annotation.GlobalTransactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class BusinessServiceImpl implements BusinessService {
+
+
+
+    @Override
+    public void purchase(String userId, String commodityCode, int orderCount) {
+        //TODO 1. 扣减库存
+
+
+        //TODO 2. 创建订单
+
+    }
+}
